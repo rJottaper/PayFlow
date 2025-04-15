fa8271c46ec70c77ee254123893f940348c19352
João Pedro <rjottaper@gmail.com>
2025-04-15 00:11:43 -0300

    Feature: PayFlow Delete Modal & Animation Login View

---
+++ PayFlow/Components/PFModalDeleteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PFModalDeleteViewController.swift
+//  PayFlow
+//
+//  Created by João Pedro Rocha on 14/04/25.
+//
+
+import UIKit
+
+class PFModalDeleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
