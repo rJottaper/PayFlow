ab9a94f55eae4e1419caeee1cb3ec2cb3a4d2148
João Pedro <rjottaper@gmail.com>
2025-05-16 00:39:00 -0300

    Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
 
 import Routes from './src/routes';
 
-const App = () => {
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+const App = () => {  
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
     'Lexend-Regular': require('./assets/fonts/Lexend-Regular.ttf'),
     'Lexend-Medium': require('./assets/fonts/Lexend-Medium.ttf'),
@@ -11,8 +19,20 @@ const App = () => {
     'Lexend-Bold': require('./assets/fonts/Lexend-Bold.ttf'),
   });
 
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.preventAutoHideAsync();
+      SplashScreen.hide();
+      setAppIsReady(true);
+    }, 2000);
+  }, []);
+
   if (!fontsLoaded) {
     return null;
+  };
+
+  if (!appIsReady) {
+    return null
   };
 
   return (
